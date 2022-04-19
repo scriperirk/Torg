3afbce641f8075cd47c368f15c9f8311cccb9a33
Scriper.irk <scriper.irk@gmail.com>
2022-04-19 23:49:47 +0800

    New14_93%

--- src/main/java/ru/netology/domain/Product.java
+++ src/main/java/ru/netology/domain/Product.java
@@ -1,8 +1,10 @@
 package ru.netology.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 
 @AllArgsConstructor
+@Data
 public class Product {
     private int id;
     private String name;
